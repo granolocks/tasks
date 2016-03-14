42eb6421567884d39b45209c862d5555c664c497
Gabe Koss <gabe@gabekoss.com>
2016-03-14 12:33:55 -0400

    start fleshing out basic CLI from bin

--- lib/tasks/task.rb
+++ lib/tasks/task.rb
@@ -5,5 +5,13 @@ class Task
   property :id, Serial
   property :title, String
   property :description, Text
+  property :priority, Text, default: 0
   property :complete, Boolean, default: false
+
+  validates_presence_of :title
+
+  def complete!
+    self.complete = true
+    self.save
+  end
 end
